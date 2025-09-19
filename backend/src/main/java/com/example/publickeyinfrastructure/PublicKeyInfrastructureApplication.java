55f8535d7d84da6740c9eb5d9640264962b8a457
DamjanVincic <damjanvincic2003@gmail.com>
2025-09-19 20:38:26 +0200

    chore: initialize backend

---
+++ backend/src/main/java/com/example/publickeyinfrastructure/PublicKeyInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.publickeyinfrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicKeyInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicKeyInfrastructureApplication.class, args);
+	}
+
+}
