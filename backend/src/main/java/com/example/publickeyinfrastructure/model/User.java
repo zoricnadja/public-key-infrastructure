fa3538690afe0708bfc8b6607491fb28beee5da6
DamjanVincic <damjanvincic2003@gmail.com>
2025-09-21 23:43:43 +0200

    feat(backend): add user model

---
+++ backend/src/main/java/com/example/publickeyinfrastructure/model/User.java
@@ -0,0 +1,36 @@
+package com.example.publickeyinfrastructure.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "\"users\"")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String keycloakId;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+}
