1e5480ac6efd7b234a52d9831b314830fcd24ca0
DamjanVincic <damjanvincic2003@gmail.com>
2025-09-23 01:01:59 +0200

    feat: backend https

--- frontend/src/enviroment/environment.ts
+++ frontend/src/enviroment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://localhost:8080/api/v1',
   keycloak: {
     url: 'http://localhost:8081',
     realm: 'pki',
