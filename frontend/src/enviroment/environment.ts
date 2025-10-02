465d52ceedda9f616b2fde0e16fc2d488397afe0
DamjanVincic <damjanvincic2003@gmail.com>
2025-10-02 23:26:52 +0200

    chore: https setup

--- frontend/src/enviroment/environment.ts
+++ frontend/src/enviroment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   apiUrl: 'https://localhost:8080/api/v1',
   keycloak: {
-    url: 'http://localhost:8081',
+    url: 'https://localhost:8443',
     realm: 'pki',
     clientId: 'pki-frontend',
   },
